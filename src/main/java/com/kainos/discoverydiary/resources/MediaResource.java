6c3e92ff0c4ac61618349a375b7a5fc0da000322
Jenkins <jenkins@localhost>
2016-11-03 14:53:39 +0000

    put dummy data to data store

--- src/main/java/com/kainos/discoverydiary/resources/MediaResource.java
+++ src/main/java/com/kainos/discoverydiary/resources/MediaResource.java
@@ -1,19 +1,12 @@
 package com.kainos.discoverydiary.resources;
 
 import com.codahale.metrics.annotation.Timed;
-import com.kainos.discoverydiary.models.Category;
-import com.kainos.discoverydiary.models.Media;
+import com.kainos.discoverydiary.DataStore;
 import com.kainos.discoverydiary.views.Detail;
-import com.kainos.discoverydiary.views.Index;
 import io.dropwizard.views.View;
-import org.assertj.core.internal.Integers;
-import org.assertj.core.internal.cglib.proxy.InvocationHandler;
-import org.joda.time.DateTime;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.util.Optional;
 
 @Path("media")
 public class MediaResource {
@@ -25,25 +18,9 @@ public class MediaResource {
 
     @GET
     @Timed
-    @Produces(MediaType.TEXT_HTML)
-    public View Index() {
-        return new Index();
-    }
-
-    @GET
-    @Timed
     @Path("{id}")
     @Produces(MediaType.TEXT_HTML)
     public View Detail(@PathParam("id") int id) {
-
-        System.out.println(id);
-
-        new Media("Array Potter 1", "J K Bowling", "Training book on java", Category.TECHNICAL, "2001");
-        new Media("Array Potter 2", "J K Bowling", "Training book on java", Category.TECHNICAL, "2001");
-        new Media("Array Potter 3", "J K Bowling", "Training book on java", Category.TECHNICAL, "2001");
-        new Media("Array Potter 4", "J K Bowling", "Training book on java", Category.TECHNICAL, "2001");
-
-        return new Detail(Media.medias.get(id));
-
+        return new Detail(DataStore.medias.get(id));
     }
 }
--- src/main/java/com/kainos/discoverydiary/views/Index.java
+++
@@ -1,7 +0,0 @@
-package com.kainos.discoverydiary.views;
-
-import io.dropwizard.views.View;
-
-public class Index extends View {
-    public Index() {super("/Views/home/index.ftl");}
-}
